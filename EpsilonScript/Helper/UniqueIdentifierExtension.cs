66d99c69b476da757b72a81eb07b55b3d715f5ea
Akihiro Noguchi <aki@aki-null.net>
2023-01-22 21:07:18 +0900

    Faster unique identifier generation

--- EpsilonScript/Helper/UniqueIdentifierExtension.cs
+++ EpsilonScript/Helper/UniqueIdentifierExtension.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading;
 
 namespace EpsilonScript.Helper
 {
@@ -8,36 +9,65 @@ namespace EpsilonScript.Helper
     private static readonly List<string> StringIdReverseLookup = new List<string>();
     private static int _currentId = 1;
 
+    private static readonly ReaderWriterLockSlim CacheLock = new ReaderWriterLockSlim();
+
     public static int GetUniqueIdentifier(this string s)
     {
-      lock (StringIdCache)
+      CacheLock.EnterUpgradeableReadLock();
+      try
       {
         if (StringIdCache.TryGetValue(s, out var id))
         {
           return id;
         }
 
-        id = _currentId;
-        StringIdCache[s] = id;
-        StringIdReverseLookup.Add(s);
-        ++_currentId;
-        return id;
+        CacheLock.EnterWriteLock();
+        try
+        {
+          id = _currentId;
+          StringIdCache[s] = id;
+          StringIdReverseLookup.Add(s);
+          ++_currentId;
+          return id;
+        }
+        finally
+        {
+          CacheLock.ExitWriteLock();
+        }
+      }
+      finally
+      {
+        CacheLock.ExitUpgradeableReadLock();
       }
     }
 
     public static string GetStringFromUniqueIdentifier(this int s)
     {
-      lock (StringIdCache)
+      CacheLock.EnterReadLock();
+      try
       {
         var lookupPos = s - 1;
         return StringIdReverseLookup[(int)lookupPos];
+      }
+      finally
+      {
+        CacheLock.ExitReadLock();
       }
     }
 
     public static void ResetUniqueIdentifierCache()
     {
-      StringIdCache.Clear();
-      _currentId = 1;
+      CacheLock.EnterWriteLock();
+      try
+      {
+        StringIdCache.Clear();
+        StringIdReverseLookup.Clear();
+        _currentId = 1;
+      }
+      finally
+      {
+        CacheLock.ExitWriteLock();
+      }
     }
   }
 }
